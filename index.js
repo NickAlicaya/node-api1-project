490e8ce01a0832ce3f394311853ebfd1cca936b2
nicanor-alicaya <donalicaya@gmail.com>
2020-01-13 19:03:51 -0500

    get requests work, yay!

--- index.js
+++ index.js
@@ -12,16 +12,18 @@ server.use(express.json())
 
 // GET to "/"
 
+//| GET    | /api/users     | Returns an array of all the user objects contained in the database. 
 server.get('/', function(request, response) {
-  response.send({ hello: 'Web 25!' });
+  response.send({ hello: 'Users' });
 });
 
-//see a list of Hubs
-server.get('/api/hubs',(req,res) => {
-// read the data from the database (Hubs)
+//see a list of Users
+server.get('/api/users',(req,res) => {
+// read the data from the database (Db)
     Users.find() //return a promise
-    .then(hubs => {
-        res.status(200).json(hubs)
+    .then(users => {
+        console.log("Users",users);
+        res.status(200).json(users)
     })
     .catch(error => {
         console.log(error);
@@ -29,6 +31,26 @@ server.get('/api/hubs',(req,res) => {
         res.status(500).json()
     })
 })
+
+//| GET    | /api/users/id    | Returns a specific user based on id. 
+ 
+  server.get('/api/users/:id',(req,res) => {
+  // read the data from the database (Db)
+        const id = req.params.id;
+
+      Users.findById(id) //return a promise
+      .then(users => {
+          console.log("User by id",users);
+          res.status(200).json(users)
+      })
+      .catch(error => {
+          console.log(error);
+          //handle the error
+          res.status(500).json()
+      })
+  })
+
+
 
 const port = 8000;
 server.listen(port, () => console.log(`\n ** api on port: ${port} ** \n`));
